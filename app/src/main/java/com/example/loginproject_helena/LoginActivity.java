35fbf8eaa666fd90c49dbe53701e10610436d90d
helenaAfshari <afshari.hln@gmail.com>
2021-09-19 17:15:41 +0430

    Delete editor.PutBoolean in class BaseApplication in Method string Shared

--- app/src/main/java/com/example/loginproject_helena/LoginActivity.java
+++ app/src/main/java/com/example/loginproject_helena/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
   SignDao signDao;
   String signModel;
 
-
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
